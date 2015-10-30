200e8b37364d74ebc499cfc957ea84cbffc966ba
unknown <sinphi000@779CTE220LAB06.alpine.local>
2015-10-30 10:31:55 -0600

    Trying to reverse.

--- Lab5_Pointers/Lab5_Pointers.cpp
+++ Lab5_Pointers/Lab5_Pointers.cpp
@@ -4,18 +4,36 @@
 #include "stdafx.h"
 #include <iostream>
 
-void reverser(char *myStr[])
-{
-	while (? ? ? )
-	{
+int size;
 
-	}
+void reverser(char myStr[])
+{
+	int start = 0;
+	int end = size - 2;
+	int fin = end / 2;
+	std::cout << end << std::endl;
+	char startToEnd;
+	char endToStart;
+	
+	do
+	{
+		startToEnd = myStr[start];
+		endToStart = myStr[end];
+		myStr[start] = endToStart;
+		myStr[endToStart] = startToEnd;
+		start++;
+		end--;
+
+	} while (start < 2);
 }
 
 int main()
 {
     // declare a char string to reverse
-	char myString[] = "Hellow World!";
+
+	char myString[] = "Hello World!";
+	size = sizeof(myString) / sizeof(myString[1]);
+	std::cout << size;
 
 	// call the reverser function
 	reverser(myString);
@@ -27,3 +45,18 @@ int main()
 	return 0;
 }
 
+/*
+void reverser(char * pointer)
+{
+	char * first_char = pointer;
+	char * last_char = pointer + strlen(pointer);
+	while (first_char = pointer < last_char)
+	{
+		--last_char;
+		char tem_char = *first-char;
+		*first_char = *last_char;
+		*last_char = temp_char;
+		++first_char;
+
+	}
+*/
