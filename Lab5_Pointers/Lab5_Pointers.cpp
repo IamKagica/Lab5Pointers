79ca99632d614d113fdf37cd0217e5e2a94e34be
unknown <sinphi000@779CTE220LAB06.alpine.local>
2015-10-30 09:24:44 -0600

    Trying to Start

---
+++ Lab5_Pointers/Lab5_Pointers.cpp
@@ -0,0 +1,29 @@
+// Lab5_Pointers.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+void reverser(char *myStr[])
+{
+	while (? ? ? )
+	{
+
+	}
+}
+
+int main()
+{
+    // declare a char string to reverse
+	char myString[] = "Hellow World!";
+
+	// call the reverser function
+	reverser(myString);
+
+	//output the result
+	std::cout << myString << std::endl;
+
+	std::system("PAUSE");
+	return 0;
+}
+
